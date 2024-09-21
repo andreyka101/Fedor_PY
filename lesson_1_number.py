f7640b517baada415656a1a99f0ae301e85255cc
andreyka101 <andrejkvasnin236@gmail.com>
2024-09-21 17:26:24 +0300

    first commit

---
+++ lesson_1_number.py
@@ -0,0 +1,82 @@
+# num = 1
+# num_2 = num + 4
+# print(num , num_2)
+
+
+
+# num = 1
+# num = num + 4
+# print(num)
+
+
+
+# num = 1
+# num = (num + 4) /2
+# print(num)
+
+
+
+# num_int = 26
+# num_float = 3.6
+# text_str = "4feff"
+# bool_num = False
+
+# print(type(bool_num))
+
+
+
+# red_cat = 9
+# redCatDog = 9
+
+
+
+# print(2*5)
+
+
+
+# num_1 = 2 + 2
+# num_2 = 2 - 2
+# num_3 = 2 * 2
+# num_4 = 2 ** 2
+
+# num_5 = 10 / 3
+# num_6 = 10 // 3
+# num_7 = 10 % 3
+# print(num_5)
+# print(num_6)
+# print(num_7)
+
+
+
+# text_1 = "text1"
+# text_2 = 'text2'
+
+# print("hel" + "lo")
+# print("hel_" * 3)
+# print(text_1 + text_2)
+
+
+
+# text_a1 = "6"
+# print(text_a1 * 4)
+# print(type(text_a1))
+
+# text_a2 = int("6")
+# print(text_a2 * 4)
+# print(type(text_a2))
+
+# text_a3 = float("6")
+# print(text_a3)
+# print(type(text_a3))
+
+# text_a4 = str(7)
+# print(text_a4 * 4)
+# print(type(text_a4))
+
+
+
+
+
+
+print("hello")
+input("-5-")
