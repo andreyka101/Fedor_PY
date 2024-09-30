73b20fea5db45511855b792831464051a126bece
andreyka101 <andrejkvasnin236@gmail.com>
2024-09-30 20:04:42 +0300

    hyhyh

---
+++ lesson_4_while_for.py
@@ -0,0 +1,90 @@
+# num=0
+# while(num < 10):
+#     print("hello")
+#     print(num)
+#     num+=1
+# print("end")
+# print("ok")
+
+
+
+# end_bool = True
+# while(end_bool):
+#     print("hello")
+#     num_inp = int(input("__"))
+#     if(0 == num_inp):
+#         end_bool = False
+# print("end")
+
+
+
+# for index in range(10):
+#     print(index)
+
+
+
+# for index in range(10):
+#     if(index % 2 == 0):
+#         print(index)
+
+
+
+# for i in range(5,10 + 1):
+#     print(i)
+
+
+
+# for i in range(5, 20, 2):
+#     print(i)
+
+
+
+# str_1="qwertyuiop[]"
+# for index in range(12):
+#     print(str_1[index])
+
+
+# str_1="qwertyuiop[]"
+# for index in range(len(str_1)):
+#     print(str_1[index])
+
+
+# str_1="qwertyuiop[]"
+# for element_str in str_1:
+#     print(element_str)
+
+
+
+
+# str_number = "1234567890"
+# str_string = "qwertyuiop"
+# if(len(str_number) == len(str_string)):
+#     str_number_string = ""
+#     for i in range(len(str_number)):
+#         str_number_string += str_number[i] + str_string[i]
+#     print(str_number_string)
+
+
+
+# str_i = ""
+# for i in range(10):
+#     str_i += str(i)
+# print(str_i)
+
+
+str_x = ""
+for y in range(10):
+    for x in range(10):
+        str_x += str(x) + str(y) + " "
+    print(str_x)
+    str_x = ""
+
+
+
+
+
+
+
+
+
+
