c1982e3d1f17f333d36a351b4374b805c25b86bd
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-07 20:02:19 +0300

    wdwdwd

---
+++ lesson_7_function.py
@@ -0,0 +1,95 @@
+# def fun_1():
+#     print("ok")
+
+# # fun_1()
+# # fun_1()
+# # fun_1()
+# # print("hello")
+# # fun_1()
+
+
+
+# num_1 = 9
+# def fun_2():
+#     print(num_1)
+# fun_2()
+
+
+
+# def fun_3():
+#     num_2 = 5
+#     print(num_2)
+# fun_3()
+# # print(num_2)
+
+
+
+# def fun_4():
+#     global num_3
+#     num_3 = 2
+#     print(num_3)
+# fun_4()
+# print(num_3)
+
+
+
+# def fun_5(a):
+#     print(a)
+
+# fun_5("pip")
+
+
+
+# def fun_6(a = 7):
+#     print(a)
+
+# fun_6(4)
+
+
+
+# def fun_6(num_a, num_b, num_s):
+#     print(num_a + num_b + num_s)
+#     print(num_a , num_b , num_s)
+
+# fun_6(4 , 3, 1)
+
+
+
+# def fun_7(num_a, num_b, num_s = 7):
+#     print(num_a + num_b + num_s)
+#     print(num_a , num_b , num_s)
+
+# fun_7(4 , 3 , 60)
+
+
+
+# def fun_8():
+#     return 1
+
+# num_4 = fun_8() + 5
+# print(num_4)
+
+
+
+# def fun_9(num_a = 1):
+#     print("start")
+#     return num_a ** 3
+#     print("end")
+
+# num_5 = fun_9(5)
+# print(num_5)
+
+
+
+# def fun_10():
+#     print("start")
+#     def fun_local():
+#         print("start_local")
+#     fun_local()
+    
+
+# fun_10()
+
+
+
+print(round(9.89334,2))
