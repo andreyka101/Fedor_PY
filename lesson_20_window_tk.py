4b929afd9f744c38c80f77b7b42ad4f22a5ca2fe
andreyka101 <andrejkvasnin236@gmail.com>
2024-11-15 19:56:46 +0300

    уаааа

---
+++ lesson_20_window_tk.py
@@ -0,0 +1,50 @@
+from tkinter import *
+
+window = Tk()
+window.title("window")
+window.geometry("600x500+200+300")
+window.config(bg="#facca6")
+
+lab = Label(text="asryffgvg", bg="#dfc062" , fg = "#6600FF")
+lab.place(x=10 , y=40)
+def fun_del():
+    # del lab
+    lab.place(x=-100 , y=-100)
+button_del = Button(text="delete Label" , command = fun_del)
+button_del.place(x=10 , y=80)
+
+
+
+inp_enter = Entry()
+inp_enter.place(x=20 , y=200)
+
+def fun_in_1():
+    print(inp_enter.get())
+    window.geometry(inp_enter.get())
+    
+    
+button_in_1 = Button(text="run" , command = fun_in_1)
+button_in_1.place(x=20 , y=230)
+
+
+
+
+
+inp_enter_color = Entry()
+inp_enter_color.place(x=200 , y=200)
+
+def fun_in_2():
+    print(inp_enter_color.get())
+    window.config(bg=inp_enter_color.get())
+    
+    
+button_in_2 = Button(text="run color" , command = fun_in_2 )
+button_in_2.place(x=200 , y=230)
+
+
+
+
+
+
+
+window.mainloop()
---
+++ lesson_21_entry_tk.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+window = Tk()
+window.title("window")
+window.geometry("600x500")
+window.config(bg="#979797")
+
+
+
+inp_enter = Entry()
+inp_enter.place(x=20 , y=20 , height=30 , width=200)
+
+
+def fun_in_1():
+    # inp_enter.insert(0, "Hello World")
+    print(inp_enter.get())
+    if(inp_enter.get() != ""):
+        lab_text.config(text=inp_enter.get())
+        lab_text.place(x=10 , y=40)
+    else:
+        lab_text.place(x=-110 , y=-110)
+    
+button_in_1 = Button(text="run" , command = fun_in_1)
+button_in_1.place(x=10 , y=80)
+
+lab_text = Label(text="none" , fg = "#350184")
+lab_text.place(x=10 , y=40)
+
+
+
+
+
+
+window.mainloop()
