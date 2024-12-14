b69db1918c6a0c5a859406ab81498ee6588c896f
andreyka101 <andrejkvasnin236@gmail.com>
2024-12-14 20:34:52 +0300

    wwfwfwff

---
+++ lesson_canvas/lesson_31_canvas_2.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+window = Tk()
+window.title("window")
+window.geometry("600x500")
+window.config(bg="#facca6")
+
+
+canV = Canvas(width=600 , height= 500 , bg="#fff")
+canV.place(x=0,y=0)
+
+
+# start – начальный угол дуги в градусах;
+# extent – размер дуги в градусах. Дуга всегда рисуется в направлении 
+# против часовой стрелки;
+# fill = цвет
+# width = толщина линии дуги
+# outline = цвет рамки
+canV.create_arc(50 , 50 , 200 , 200, fill="#701717" , start = 50 , extent = 410)
+
+
+window.mainloop()
--- lesson_canvas/mouse_canvas.py
+++ lesson_canvas/mouse_canvas.py
@@ -9,6 +9,7 @@ window.config(bg="#facca6")
 canV = Canvas(width=600 , height= 500 , bg="#fff")
 canV.place(x=0,y=0)
 
+# список координат для квадратов
 arr_but = [
     {
         "x1":30,
@@ -25,10 +26,11 @@ arr_but = [
         "hover":True,
     },
 ]
+
+# рисуем квадрат и текст
 for el in arr_but:
     canV.create_rectangle(el["x1"] , el["y1"] , el["x2"] , el["y2"], fill="#f21a1a", width=0)
-    canV.create_text(el["x1"] + 30 , el["y1"] + 30  ,text = "Hello" ,font="Boli 15" , fill="#184112" , justify="center")
-
+    # canV.create_text(el["x1"] + 30 , el["y1"] + 30  ,text = "Hello" ,font="Boli 15" , fill="#184112" , justify="center")
 
 
 
@@ -36,9 +38,11 @@ def fun_b1(event):
     for el in arr_but:
         print(el)
         print("====")
+        # при нажатии на квадрат меняем ему цвет
         if((el["x1"] <= event.x and el["x2"] >= event.x) and (el["y1"] <= event.y and el["y2"] >= event.y)):
             canV.create_rectangle(el["x1"] , el["y1"] , el["x2"] , el["y2"], fill="#1aebf2", width=0)
             el["hover"] = False
+        # при нажатии на пустое место (вне квадрата) меняем ему цвет обратно 
         if((el["x1"] >= event.x or el["x2"] <= event.x) or (el["y1"] >= event.y or el["y2"] <= event.y)):
             canV.create_rectangle(el["x1"] , el["y1"] , el["x2"] , el["y2"], fill="#f21a1a", width=0)
             el["hover"] = True
@@ -49,11 +53,12 @@ canV.bind("<Button-1>" , fun_b1)
 def mouse_motion(event):
     window.title(str(event.x) + " / " + str(event.y))
     for el in arr_but:
+        # при наведении на квадрат меняем ему цвет если он не нажат
         if(((el["x1"] <= event.x and el["x2"] >= event.x) and (el["y1"] <= event.y and el["y2"] >= event.y)) and el["hover"]):
             canV.create_rectangle(el["x1"] , el["y1"] , el["x2"] , el["y2"], fill="#e7ce44", width=0)
+        # при наведении на пустое место (вне квадрата) меняем ему цвет обратно если он не нажат
         if(((el["x1"] >= event.x or el["x2"] <= event.x) or (el["y1"] >= event.y or el["y2"] <= event.y)) and el["hover"]):
             canV.create_rectangle(el["x1"] , el["y1"] , el["x2"] , el["y2"], fill="#f21a1a", width=0)
-          
 canV.bind("<Motion>" , mouse_motion)
 
 window.mainloop()
