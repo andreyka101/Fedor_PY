4e2355550030928475a838b2d3e5b04599d8f668
andreyka101 <andrejkvasnin236@gmail.com>
2024-12-17 17:46:04 +0300

    уаууа

--- lesson_canvas/paint.py
+++ lesson_canvas/paint.py
@@ -9,18 +9,28 @@ window.config(bg="#facca6")
 canV = Canvas(width=600 , height= 500 , bg="#fff")
 canV.place(x=0,y=0)
 
+# индикатор нажатия кнопки
 num_bool = False
 
-def fun_b1(e):
+
+# движение мыши
+def fun_b1(event):
     global num_bool
+    # print(num_bool)
+    # если кнопка нажата
     if(num_bool):
-        canV.create_oval(e.x , e.y , e.x + 20 , e.y + 20 , fill="#232323" ,width=0 , outline="#1da7e2")
+        # создаём oval
+        canV.create_oval(event.x , event.y , event.x + 20 , event.y + 20 , fill="#232323" ,width=0 , outline="#1da7e2")
 canV.bind("<Motion>" , fun_b1)
 
+
+# нажатие кнопки
 def fun_bPress(event):
     global num_bool
+    # print(event.keysym)
     if(event.keysym == "z"):
         num_bool = True
-canV.bind("<KeyPress>" , fun_bPress)
+# !!! здесь была моя ошибка
+window.bind("<KeyPress>" , fun_bPress)
 
 window.mainloop()
